8f65afe5d7f91f27005575b39e58859d384a1edb
Ben Junya <teknal@teknal.studio>
2026-09-18 12:32:37 -0700

    Exclude worktrees from tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     globalSetup: ["./tests/global-setup.ts"],
     environment: "node",
     coverage: { enabled: false },
+    exclude: [...configDefaults.exclude, "**/.worktrees/**"],
   },
 });
